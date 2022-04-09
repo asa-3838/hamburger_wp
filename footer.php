5e746d01431fde22749f1c33f1ec078c11fab534
DESKTOP-3SDFCG5\sayak <sayaka.uu.3838@gmail.com>
2022-04-09 18:31:43 +0900

    サブメニューのCSS調整

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="l-footer">
   <div class="p-footer__copy-right">
-    <p class="p-footer-shop"><a href="#">ショップ情報</a></p>
-    <p class="p-footer-history"><a href="#">ヒストリー</a></p>
+    <p class="p-footer-shop"><a href="<?php echo esc_url(home_url('/shop')); ?>">ショップ情報</a></p>
+    <p class="p-footer-history"><a href="<?php echo esc_url(home_url('/history')); ?>">ヒストリー</a></p>
     <small>Copyright: RaiseTech</small>
   </div>
 </footer>
--- sidebar.php
+++ sidebar.php
@@ -2,17 +2,18 @@
   <h2 class="p-sidebar__title">Menu</h2>
   <button id ="close_btn" class="p-sidebar__button"></button>
   <nav class="p-sidebar__main-nav">
-  <ul class="p-sidebar__main-ul">
 
-      <?php wp_nav_menu(
-          array(
-            'theme_location' => 'sidebar-menu',
-            'container' => '',
-            'menu_class' => 'p-sidebar__main-ul'
+  <?php
+      echo str_replace('sub-menu', 'p-sidebar__sub-ul', wp_nav_menu(
+        array(
+              'echo' => false,
+              'theme_location' => 'sidebar-menu',
+              'container' => '',
+              'menu_class' => 'p-sidebar__main-ul'
+            ) 
           )
         );
       ?>
-
-    </ul>
+      
   </nav>
 </aside>
