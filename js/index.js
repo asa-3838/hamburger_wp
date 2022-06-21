c65ded25d1d937f8dcd1a8812fd871709da580d6
DESKTOP-3SDFCG5\sayak <sayaka.uu.3838@gmail.com>
2022-06-21 09:10:05 +0900

    js修正

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
 jQuery("#menu_btn").click(function () {
+  jQuery("body").toggleClass("no-scroll");
   jQuery(".l-main__back").toggleClass("is-open");
   jQuery(".l-main__right").toggleClass("is-open");
   jQuery(".p-sidebar__button").toggleClass("is-open");
-  jQuery("body").toggleClass("no-scroll");
+  
 });
 //×ボタンをクリックすれば、クラスis-openを削除し、サイドバー展開
 jQuery("#close_btn").click(function () {
+  jQuery("body").toggleClass("no-scroll");
   jQuery(".l-main__back").toggleClass("is-open");
   jQuery(".l-main__right").toggleClass("is-open");
   jQuery(".p-sidebar__button").toggleClass("is-open");
-  jQuery("body").toggleClass("no-scroll");
 });
 
-jQuery(window).on('resize', function(){
+jQuery(window).on('resize load', function(){
   if (jQuery(".l-main__right").hasClass("is-open")) {
-  jQuery(".l-main__back").removeClass("is-open");
-  jQuery(".l-main__right").removeClass("is-open");
-  jQuery(".p-sidebar__button").removeClass("is-open");
-  jQuery("body").remove("no-scroll");
+    jQuery("body").remove("no-scroll");
+    jQuery(".l-main__back").removeClass("is-open");
+    jQuery(".l-main__right").removeClass("is-open");
+    jQuery(".p-sidebar__button").removeClass("is-open");
+    location.reload();
   }
 });
